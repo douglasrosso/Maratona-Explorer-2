346b512ab89b374c05c40e0d467ca342b0115d62
PsychoJantis <107089633+PsychoJantis@users.noreply.github.com>
2022-06-18 00:37:52 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const elementoResultados = document.querySelector("#resultado")
+const resultados = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+]
+function sortearDado() {
+botaoSortear.setAttribute("disabled", true)
+const resultado = "<div>" + sortearDado.value + "</div>"
+const totalResultados = resultados.length
+const numeroAleatorio = Math.floor(Math.random() * totalResultados)
+ console.log(resultados[numeroAleatorio])
+elementoResultados.innerHTML = resultados[numeroAleatorio]
+elementoResultados.style.opacity = 1;
+setTimeout(function() {
+    elementoResultados.style.opacity = 0;
+    botaoSortear.removeAttribute("disabled")
+  }, 3000)
+}
+function linkCriador() {
+  window.location.href = "https://github.com/PsychoJantis";
+}
